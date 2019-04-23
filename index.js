b15a9a003ade5e67fe8c48a78588049e29a8c722
Kyle Walters <kyle.p.walters@gmail.com>
2019-04-23 17:53:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,19 +14,12 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string) {
-  return string.toLowerCase()
-}
 
 function sayHiToGrandma(string) {
-  return string.toUpperCase()
-}
-
-function sayHiToGrandma(string) {
-  if ((string) === string.toLowerCase())
+  if (string === string.toLowerCase())
   return "I can't hear you"
-  if ((string) === string.toUpperCase())
+  if (string === string.toUpperCase())
   return "YES INDEED!"
-  if ((string) === "I love you, Grandma.")
+  if (string === "I love you, Grandma.")
   return "I love you, too."
 }
